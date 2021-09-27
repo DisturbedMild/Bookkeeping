5c749bd266a67782f555dc91aea234a308ef1a27
dmytrenkov-cience <vladyslav.dmytrenko@cience.com>
2021-09-27 14:33:49 +0300

    basic template

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,46 @@
+<template>
+ <footer id="footer">
+  <div class="container">
+   <div class="footer-copyright">
+    <div class="footer-link">
+     <a href="https://github.com/DisturbedMild" target="_blank"
+      ><img src="../../assets/icons/github.png" alt="GitHub"
+     /></a>
+    </div>
+    <div class="footer-author">Created by - Vladyslav Dmytrenko</div>
+   </div>
+  </div>
+ </footer>
+</template>
+
+<style>
+
+#footer {
+    font-size: 16px;
+    line-height: 24px;
+    font-family: Arial, sans-serif;
+    font-weight: 600;
+    margin-top: 50px;
+}
+
+.footer-copyright {
+ max-width: 320px;
+ width: 100%;
+ margin: 0 auto;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+}
+.footer-link {
+ width: 32px;
+ height: 32px;
+}
+.footer-link a img {
+ width: auto;
+ height: 100%;
+}
+
+.footer-author {
+    display: inline-block;
+}
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header id="header">
+        <div class="container">
+            <h1 class="header-text">Bookkeeping</h1>
+        </div>
+    </header>
+</template>
+
+<style>
+    #header {
+        text-align: center;
+        font-size: 32px;
+        line-height: 42px;
+        margin-bottom: 50px;
+        font-family: Arial, sans-serif;
+
+    }
+</style>
